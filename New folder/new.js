3a70c2d0685eda11f8b7a96f78f9fc8453199ced
unknown <aniket_chowdhury@hotmail.com>
2019-02-13 18:36:05 +0530

    traan

---
+++ New folder/new.js
@@ -0,0 +1,87 @@
+$("#activateForm").click(function() {
+  $("#supaForm").addClass("formActive");
+  $("#step1").addClass("stepOn");
+  $("#step1 input").focus();
+  return false;
+});
+$("#step1 .nextArrow").click(function() {
+  $("#step1").addClass("stepOff");
+  $("#step2").addClass("stepOn");
+  $("#step2 input").focus();
+  $("hr").addClass("quarter");
+});
+$("#step2 .nextArrow").click(function() {
+  $("#step2").addClass("stepOff");
+  $("#step3").addClass("stepOn");
+  $("#step3 textarea").focus();
+  $("#supaForm").addClass("messageStep");
+  $(".firstTitle").attr("id", "removeTitle");
+  $(".secondTitle").addClass("addTitle");
+  $(".subtitle").attr("id", "removeTitle");
+  $(".secondSubtitle").addClass("addSubtitle");
+  $("hr").addClass("threeQuarters");
+});
+$("#step3 .nextArrow").click(function() {
+  $("#supaForm").addClass("messageAway");
+  $(".secondTitle").attr("id", "removeTitle");
+  $(".thirdTitle").addClass("addTitleThird");
+  $(".secondSubtitle").attr("id", "removeTitle");
+  $("hr").addClass("full");
+  $(".rocketContainer").addClass("liftOff");
+  $(".skyBG").attr("id", "activeSky");
+  $(".clouds").attr("id", "activeClouds");
+});
+
+jQuery(document).on("keydown", "#step1 input", function(ev) {
+  if (ev.which === 13) {
+    $("#step1").addClass("stepOff");
+    $("#step2").addClass("stepOn");
+    $("#step2 input").focus();
+    $("hr").addClass("quarter");
+
+    return false;
+  }
+});
+jQuery(document).on("keydown", "#step2 input", function(ev) {
+  if (ev.which === 13) {
+    $("#step2").addClass("stepOff");
+    $("#step3").addClass("stepOn");
+    $("#step3 textarea").focus();
+    $("#supaForm").addClass("messageStep");
+    $(".firstTitle").attr("id", "removeTitle");
+    $(".secondTitle").addClass("addTitle");
+    $(".subtitle").attr("id", "removeTitle");
+    $(".secondSubtitle").addClass("addSubtitle");
+    $("hr").addClass("threeQuarters");
+
+    return false;
+  }
+});
+
+$("#step1 .inputText").keyup(function() {
+  var searchQuery = $(this).val();
+
+  if (searchQuery === "") {
+    $("#step1 .nextArrow").removeClass("displayBlock");
+  } else {
+    $("#step1 .nextArrow").addClass("displayBlock");
+  }
+});
+$("#step2 .inputText").keyup(function() {
+  var searchQuery = $(this).val();
+
+  if (searchQuery === "") {
+    $("#step2 .nextArrow").removeClass("displayBlock");
+  } else {
+    $("#step2 .nextArrow").addClass("displayBlock");
+  }
+});
+$("#step3 textarea").keyup(function() {
+  var searchQuery = $(this).val();
+
+  if (searchQuery === "") {
+    $("#step3 .nextArrow").removeClass("displayBlock");
+  } else {
+    $("#step3 .nextArrow").addClass("displayBlock");
+  }
+});
